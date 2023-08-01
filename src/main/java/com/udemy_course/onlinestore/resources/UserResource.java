0b4b04073b1573668bb6600952e602a8b60a4f37
Will Apolinário <willieapolinario@gmail.com>
2023-08-01 09:42:22 -0300

    Spring component registration, findById

--- src/main/java/com/udemy_course/onlinestore/resources/UserResource.java
+++ src/main/java/com/udemy_course/onlinestore/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.udemy_course.onlinestore.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy_course.onlinestore.entities.User;
+import com.udemy_course.onlinestore.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "1234156", "84 95471321");
+    @Autowired
+    UserService service;
 
-        return ResponseEntity.ok().body(u);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/udemy_course/onlinestore/services/UserService.java
@@ -0,0 +1,27 @@
+package com.udemy_course.onlinestore.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy_course.onlinestore.entities.User;
+import com.udemy_course.onlinestore.repositories.UserRepository;
+
+//@Component
+@Service // registrando como component no spring para poder ser injetado como dependência
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+
+        return obj.get();
+    }
+}
