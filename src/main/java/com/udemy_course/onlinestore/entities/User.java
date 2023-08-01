966b522f388ad5a34da2cffe61e5044c379ce4a2
Will Apolinário <willieapolinario@gmail.com>
2023-08-01 10:11:54 -0300

    Order entity, order and client entities association

--- src/main/java/com/udemy_course/onlinestore/entities/User.java
+++ src/main/java/com/udemy_course/onlinestore/entities/User.java
@@ -1,11 +1,14 @@
 package com.udemy_course.onlinestore.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -49,6 +55,10 @@ public class User implements Serializable {
 
     public String getEmail() {
         return email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setEmail(String email) {
