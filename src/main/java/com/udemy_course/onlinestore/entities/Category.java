b162a3119d20ec7ae087221a9abc494028f1c91e
Will Apolinário <willieapolinario@gmail.com>
2023-08-02 17:03:44 -0300

    ManyToMany association with JoinTable between Product and Category entities

--- src/main/java/com/udemy_course/onlinestore/entities/Category.java
+++ src/main/java/com/udemy_course/onlinestore/entities/Category.java
@@ -1,11 +1,16 @@
 package com.udemy_course.onlinestore.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +21,10 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -47,6 +56,10 @@ public class Category implements Serializable {
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/udemy_course/onlinestore/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.udemy_course.onlinestore.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy_course.onlinestore.entities.Product;
+import com.udemy_course.onlinestore.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+}
