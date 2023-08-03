f379f6496a0802b7d8594be1149170110f658770
Will Apolinário <willieapolinario@gmail.com>
2023-08-03 10:32:31 -0300

    Payment entity one-to-one association with Order

--- src/main/java/com/udemy_course/onlinestore/config/TestConfig.java
+++ src/main/java/com/udemy_course/onlinestore/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy_course.onlinestore.entities.Category;
 import com.udemy_course.onlinestore.entities.Order;
 import com.udemy_course.onlinestore.entities.OrderItem;
+import com.udemy_course.onlinestore.entities.Payment;
 import com.udemy_course.onlinestore.entities.Product;
 import com.udemy_course.onlinestore.entities.User;
 import com.udemy_course.onlinestore.entities.enums.OrderStatus;
@@ -56,6 +57,11 @@ public class TestConfig implements CommandLineRunner {
                 Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
                 Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
                 orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+                Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+                o1.setPayment(pay1);
+
+                orderRepository.save(o1);
 
                 Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.",
                                 90.5,
--- src/main/java/com/udemy_course/onlinestore/entities/Order.java
+++ src/main/java/com/udemy_course/onlinestore/entities/Order.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import com.udemy_course.onlinestore.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +35,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // operações em cascata e mesmo id entre Order e Payment
+    private Payment payment;
 
     public Order() {
     }
@@ -78,6 +83,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
