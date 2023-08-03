a1710602a2087c64b75c191bf2f189863f856fbb
Will Apolinário <willieapolinario@gmail.com>
2023-08-03 09:32:31 -0300

    OrderItem repository, order item seed

--- src/main/java/com/udemy_course/onlinestore/config/TestConfig.java
+++ src/main/java/com/udemy_course/onlinestore/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.udemy_course.onlinestore.entities.Category;
 import com.udemy_course.onlinestore.entities.Order;
+import com.udemy_course.onlinestore.entities.OrderItem;
 import com.udemy_course.onlinestore.entities.Product;
 import com.udemy_course.onlinestore.entities.User;
 import com.udemy_course.onlinestore.entities.enums.OrderStatus;
 import com.udemy_course.onlinestore.repositories.CategoryRepository;
+import com.udemy_course.onlinestore.repositories.OrderItemRepository;
 import com.udemy_course.onlinestore.repositories.OrderRepository;
 import com.udemy_course.onlinestore.repositories.ProductRepository;
 import com.udemy_course.onlinestore.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 
         @Autowired
         private ProductRepository productRepository;
+
+        @Autowired
+        private OrderItemRepository orderItemRepository;
 
         // Será executada quando o servidor iniciar
         @Override
@@ -73,6 +78,13 @@ public class TestConfig implements CommandLineRunner {
                 p4.getCategories().add(cat3);
                 p5.getCategories().add(cat2);
                 productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+                OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+                OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+                OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+                OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+                orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
         }
 }
