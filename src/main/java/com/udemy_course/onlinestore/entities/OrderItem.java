c9104f2c42e826967378c50c7ae8eba256a62657
Will Apolinário <willieapolinario@gmail.com>
2023-08-03 10:01:12 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/udemy_course/onlinestore/entities/OrderItem.java
+++ src/main/java/com/udemy_course/onlinestore/entities/OrderItem.java
@@ -37,7 +37,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
-    public Product getProduc() {
+    public Product getProduct() {
         return id.getProduct();
     }
 
